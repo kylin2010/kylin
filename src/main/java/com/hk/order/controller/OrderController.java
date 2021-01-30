5a8c63483323cdf111ffcb8b12287eb4b6d76a52
kylin <1303426717@qq.com>
2021-01-30 11:47:40 +0800

    数据库相关的

--- src/main/java/com/hk/order/controller/OrderController.java
+++ src/main/java/com/hk/order/controller/OrderController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -37,6 +38,11 @@ public class OrderController {
     public String addOrder(Order order){
         log.info("进入addOrder");
         orderService.save(order);
+        return "ok";
+    }
+
+    public String deleteOrder(@RequestParam("orderId") Integer orderId){
+        orderService.removeById(orderId);
         return "ok";
     }
 }
