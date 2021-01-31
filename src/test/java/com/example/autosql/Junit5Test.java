3f9bf15a7182972e7ccc3032c89aa46675d1b77a
kylin <1303426717@qq.com>
2021-01-31 19:44:29 +0800

    所有测试完成

--- src/test/java/com/example/autosql/Junit5Test.java
+++ src/test/java/com/example/autosql/Junit5Test.java
@@ -3,10 +3,13 @@ package com.example.autosql;
 import org.junit.jupiter.api.*;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.concurrent.TimeUnit;
+
 /**
  * @author kylin
  * @description junit5单元测试
  * @date 2021-01-31 18:02
+ * springtest可以从容器中国取
  */
 @DisplayName("可以给每个类加名字")
 @SpringBootTest
@@ -18,6 +21,7 @@ public class Junit5Test {
         System.out.println("正则测试中1");
     }
 
+    @Disabled
     @Test
     @DisplayName("测试名字2")
     void testDisplayName(){
@@ -42,5 +46,21 @@ public class Junit5Test {
     @AfterAll
     static void bafterAll(){
         System.out.println("所有测试已经结束了");
+    }
+
+    /**
+     * 设置超时时间
+     * @throws InterruptedException
+     */
+    @Timeout(value = 500,unit = TimeUnit.MILLISECONDS)
+    @Test
+    void testTimeOut() throws InterruptedException {
+        Thread.sleep(600);
+    }
+
+    @RepeatedTest(5)
+    @Test
+    void testRepeat(){
+        System.out.println("5");
     }
 }
